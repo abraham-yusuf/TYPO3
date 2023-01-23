fd91070124416892c92fd1a27a71abdf6c50e5fb
abraham-yusuf <1703020028@students.unis.ac.id>
2023-01-23 11:09:13 +0000

    init

---
+++ README.md
@@ -0,0 +1,60 @@
+# TYPO3 CMS Base Distribution
+
+Get going quickly with TYPO3 CMS.
+
+## Prerequisites
+
+* PHP 8.1
+* [Composer](https://getcomposer.org/download/)
+
+## Quickstart
+
+* `composer create-project typo3/cms-base-distribution project-name ^12`
+* `cd project-name`
+
+### Setup
+
+To start an interactive installation, you can do so by executing the following
+command and then follow the wizard:
+
+```bash
+composer exec typo3 setup
+```
+
+### Setup unattended (optional)
+
+If you're a more advanced user, you might want to leverage the unattended installation.
+To do this, you need to execute the following command and substitute the arguments
+with your own environment configuration.
+
+```bash
+composer exec typo3 setup \
+    --no-interaction \
+    --driver=mysqli \
+    --username=typo3 \
+    --password=typo3 \
+    --host=127.0.0.1 \
+    --port=3306 \
+    --dbname=typo3 \
+    --admin-username=admin \
+    --admin-email="info@typo3.org" \
+    --admin-user-password=password \
+    --project-name="My TYPO3 Project" \
+    --create-site="https://localhost/"
+```
+
+### Development server
+
+While it's advised to use a more sophisticated web server such as
+Apache 2 or Nginx, you can instantly run the project by using PHPs` built-in
+[web server](https://secure.php.net/manual/en/features.commandline.webserver.php).
+
+* `TYPO3_CONTEXT=Development php -S localhost:8000 -t public`
+* open your browser at "http://localhost:8000"
+
+Please be aware that the built-in web server is single threaded and only meant
+to be used for development.
+
+## License
+
+GPL-2.0 or later
---
+++ config/system/settings.php
@@ -0,0 +1,109 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$QS5UNXdtQWszVGVsWDB6Tw$JjB9E6xqFEBT1p0ecLwmMDpxjfBi/8PfImyQrqshS9Y',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '9a6a8d6fb1de465184722582c5e73511ab0e2ab066684d3cf42f953822befe9fccc08f0511dd24ccc337f4f3f882f1a1',
+        'exceptionalErrors' => 4096,
+        'sitename' => 'TYPO3',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
